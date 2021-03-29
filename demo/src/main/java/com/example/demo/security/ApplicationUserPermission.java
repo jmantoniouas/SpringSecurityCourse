108f738b01d33a38fbf8f44ac2bf62bf08dbef1e
Joshua Antonio <jmantonio@uas.com.ph>
2021-03-29 19:59:03 +0800

    Initial commit for Spring Security Course

---
+++ demo/src/main/java/com/example/demo/security/ApplicationUserPermission.java
@@ -0,0 +1,18 @@
+package com.example.demo.security;
+
+public enum ApplicationUserPermission {
+    STUDENT_READ("student_read"),
+    STUDENT_WRITE("student_write"),
+    COURSE_READ("course_read"),
+    COURSE_WRITE("course_write");
+
+    private final String permission;
+
+    ApplicationUserPermission(String permission) {
+        this.permission = permission;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+}
